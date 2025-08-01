07f9dcee98b75e47184262244036502e4d8ea031
Blank-Truth <v01ds4v4nt001@gmail.com>
2025-08-01 22:53:44 +0100

    2nd commit

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,14 +1,14 @@
 def perform_operation(num1, num2, operation):
     num1 = float(input("Enter the first number"))
     num2 = float(input("Enter the second number"))
-    operation = input("Enter an operation i.e: + - * /").strip()
-    if operation == "+":
+    operation = input("Enter an operation e.g: add, subtract, multiply, divide").strip()
+    if operation == "add":
         return num1 + num2 
-    elif operation == "-":
+    elif operation == "subtract":
         return num1 - num2
-    elif operation == "*":
+    elif operation == "multiply":
         return num1 * num2
-    elif operation == "/":
+    elif operation == "divide" and num1/num2 != 0:
         return num1 / num2
     else:
         print("invalid operator, try again")
