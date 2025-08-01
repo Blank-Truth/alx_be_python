7673ac2cdd531b2624beac755725f0616bae29f4
Blank-Truth <v01ds4v4nt001@gmail.com>
2025-08-01 22:55:19 +0100

    3rd commit

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -8,7 +8,7 @@ def perform_operation(num1, num2, operation):
         return num1 - num2
     elif operation == "multiply":
         return num1 * num2
-    elif operation == "divide" and num1/num2 != 0:
+    elif operation == "divide" and num2 != 0:
         return num1 / num2
     else:
         print("invalid operator, try again")
