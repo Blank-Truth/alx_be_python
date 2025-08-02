c8902426536f58512ca1b53dc912d85b1460f802
Blank-Truth <v01ds4v4nt001@gmail.com>
2025-08-02 04:13:06 +0100

    Shopping_list 5th commit

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -10,18 +10,18 @@ def main():
     shopping_list = []
     while True:
         display_menu()
-        choice = map(int, input("Enter your choice(i.e 1-4): ").strip())
-        if choice == 1:
+        choice = input("Enter your choice(i.e 1-4): ").strip()
+        if choice == '1':
             item = input("Enter item you want to add").strip()
             shopping_list.append(item)
             print(shopping_list)
-        elif choice == 2:
+        elif choice == '2':
             item = input("Enter item you want to remove").strip()
             shopping_list.remove(item)
             print(shopping_list)
-        elif choice == 3:
+        elif choice == '3':
             print(shopping_list)
-        elif choice == 4:
+        elif choice == '4':
             print("Goodbye!")
             break
         else:
