fee8114577eca2b90812cbb872d62f40eefdafc2
Blank-Truth <v01ds4v4nt001@gmail.com>
2025-08-13 15:15:42 +0100

    2nd commit

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,15 @@
+num1 = int(input("Enter the first number:"))
+num2 = int(input("Enter the second number:"))
+operation = input("Choose the operation (+, -, *, /):")
+
+match operation:
+    case "+":
+        print(f"The result is {num1 + num2}")
+    case "-":
+        print(f"The result is {num1 - num2}")
+    case "*":
+        print(f"The result is {num1 * num2}")
+    case x if x == "/" and num2 != 0:
+        print(f"The result is {num1 / num2}")
+    case x if x == "/" and num2 == 0:
+        print("Cannot divide by Zero")
